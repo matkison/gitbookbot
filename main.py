3d49a0ef5ceca72f60d5b45afa7e642d50776f63
matkison <atkisonmatthew25@gmail.com>
2024-07-08 14:27:08 -0500

    edited main.py for the third time

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 def main():
-	
+	print("attempting to open the file...")
 	with open ("books/frankenstein.txt") as f:
 		# this saves the frankenstein.txt as f I guess
 		file_contents = f.read()
+		print("file contents read sucessfully")
 	print(file_contents)
 main()
 
